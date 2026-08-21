cc237dce82cce653694171e9dd043c1b80205941
Kristian Frossos <55474756+Frosk-Kristian@users.noreply.github.com>
2026-08-21 18:11:21 +0800

    Update watcher.py
    Updated docstring formatting to match Google (VSCode renders this a bit better).
    Removed some of the print statements I had while initially learning to use watchdog.
    Changed input validation to raise an exception instead of printing a message and returning, main.py will be adjusted to catch this exception.

--- Modules/watcher.py
+++ Modules/watcher.py
@@ -4,8 +4,8 @@ from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
 class MyEventHandler(FileSystemEventHandler):
-    def on_any_event(self, event: FileSystemEvent) -> None:
-        print(f"I saw something: {event}")
+    #def on_any_event(self, event: FileSystemEvent) -> None:
+        #print(f"I saw something: {event}")
 
     def on_created(self, event: FileSystemEvent) -> None:
         print(f"I see something new: {event} (created)")
@@ -13,21 +13,32 @@ class MyEventHandler(FileSystemEventHandler):
     def on_deleted(self, event: FileSystemEvent) -> None:
         print(f"I no longer see: {event} (deleted)")
 
+    def on_modified(self, event: FileSystemEvent) -> None:
+        print(f"I saw something change: {event} (modified)")
+
 def watcher(dirs: list[str]):
+    """
+    Accepts a list of directories to be watched for file changes and schedules an observer to monitor them.
+    Args:
+        dirs: list of string directories to be monitored
+    Raises:
+        ValueError: if dirs is empty
+    """
+    # checks for empty lists
     if not dirs:
-        print("ERROR: nothing to watch!")
-        return
+        raise ValueError("no valid directories provided!")
     
     handler = MyEventHandler()
     observer = Observer()
 
+    # schedules the observer to watch each directory in dirs
     for dir in dirs:
-        print(f"I will watch: \"{dir}\"")
         observer.schedule(handler, dir, recursive=True)
     
-    print("Watching...")
     observer.start()
 
+    # runs until interrupted by the user (Ctrl+C)
+    print("Watching for file changes, press Ctrl+C to stop.")
     try:
         while True:
             time.sleep(1)

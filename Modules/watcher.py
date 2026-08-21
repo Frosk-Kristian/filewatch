b52c96df041eb74a22aca8332d550f71b130999a
Kristian Frossos <55474756+Frosk-Kristian@users.noreply.github.com>
2026-08-21 17:42:56 +0800

    Update watcher.py
    Initial watcher class, currently just prints whatever events it sees

--- Modules/watcher.py
+++ Modules/watcher.py
@@ -1,3 +1,38 @@
+import time
 
-def watcher(dirs):
-    pass
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        print(f"I saw something: {event}")
+
+    def on_created(self, event: FileSystemEvent) -> None:
+        print(f"I see something new: {event} (created)")
+
+    def on_deleted(self, event: FileSystemEvent) -> None:
+        print(f"I no longer see: {event} (deleted)")
+
+def watcher(dirs: list[str]):
+    if not dirs:
+        print("ERROR: nothing to watch!")
+        return
+    
+    handler = MyEventHandler()
+    observer = Observer()
+
+    for dir in dirs:
+        print(f"I will watch: \"{dir}\"")
+        observer.schedule(handler, dir, recursive=True)
+    
+    print("Watching...")
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Interrupted, stopping...")
+    finally:
+        observer.stop()
+        observer.join()

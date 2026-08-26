f4d11d498cd48c131e66bf596f7de11c400a3d02
Kristian Frossos <55474756+Frosk-Kristian@users.noreply.github.com>
2026-08-26 16:07:56 +0800

    Update watcher.py
    Modified watcher so that a separate observer is registered for each different drive among monitored directories, as the previous solution would not work when attempting to monitor multiple directories across different drives (i.e. one on C: and another on D:).
    Now takes a dictionary argument, with the key representing a drive and the value being a list of directories on that drive to be monitored.

--- Modules/watcher.py
+++ Modules/watcher.py
@@ -28,26 +28,36 @@ class MyEventHandler(FileSystemEventHandler):
         print(f"I saw something move: {event} (moved)")
         insert_db(event.src_path, event.dest_path, "move")
 
-def watcher(dirs: list[str]):
+def watcher(dirs: dict[str, list[str]]):
     """
-    Accepts a list of directories to be watched for file changes and schedules an observer to monitor them.
+    Accepts a dictionary of directories, grouped by disk drive, and registers an observer to each drive to watch for file changes. 
+    If all directories are on the same drive, just supply a dictionary with a single key-value pair. 
+    The actual value of the key(s) shouldn't matter, as it's just used to group directories under the same observer.
     Args:
-        dirs: list of string directories to be monitored
+        dirs: dictionary with string disk drives as keys and lists of string directories as values
     Raises:
         ValueError: if dirs is empty
     """
-    # checks for empty lists
+    # checks if dictionary is empty
     if not dirs:
         raise ValueError("no valid directories provided!")
-    
-    handler = MyEventHandler()
-    observer = Observer()
 
-    # schedules the observer to watch each directory in dirs
-    for dir in dirs:
-        observer.schedule(handler, dir, recursive=True)
+    # event handler instance
+    handler = MyEventHandler()
+    # list of observers
+    observers = []
+
+    # registers a new observer for each disk in the supplied list of directories
+    for disk in dirs.keys():
+        observer = Observer()
+
+        # schedules the observer to watch each directory in dirs
+        for dir in dirs[disk]:
+            print(f"I will watch: {dir}")
+            observer.schedule(handler, dir, recursive=True)
     
-    observer.start()
+        observer.start()
+        observers.append(observer)
 
     # runs until interrupted by the user (Ctrl+C)
     print("Watching for file changes, press Ctrl+C to stop.")
@@ -57,5 +67,9 @@ def watcher(dirs: list[str]):
     except KeyboardInterrupt:
         print("Interrupted, stopping...")
     finally:
-        observer.stop()
-        observer.join()
+        # signals to each thread to shut down (non-blocking)
+        for observer in observers:
+            observer.stop()
+        # waits for each thread to actually finish (blocking)
+        for observer in observers:
+            observer.join()
